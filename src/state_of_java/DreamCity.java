414e7cf51072018545f04703782eee8b7c6949b6
Mariia <kitmaria.2015@gmail.com>
2022-09-05 18:27:06 +0300

    declared and initialized variables

--- src/state_of_java/DreamCity.java
+++ src/state_of_java/DreamCity.java
@@ -1,4 +1,11 @@
 package state_of_java;
 
 public class DreamCity {
+    int population;
+    static double area = 765_967;
+    String language;
+    public DreamCity(int population, String language){
+        this.population = population;
+        this.language = language;
+    }
 }
