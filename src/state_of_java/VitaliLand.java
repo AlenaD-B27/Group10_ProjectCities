1e790ae53e0b6df96ba98def244ede8cdca7f564
Vitalii <vtrushch@gmail.com>
2022-09-05 22:33:43 -0400

    add variables VitaliLand

--- src/state_of_java/VitaliLand.java
+++ src/state_of_java/VitaliLand.java
@@ -1,4 +1,15 @@
 package state_of_java;
 
 public class VitaliLand {
+
+    int population;
+    double area;
+    String language;
+
+  public VitaliLand(int population, double area, String language){
+    this.population = population;
+    this.area = area;
+    this.language = language;
+  }
+
 }
