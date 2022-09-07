5ff7f326b79d87fdbc3af6af148c1b08c17bd9ca
AlenaD-B27 <lenador.18@gmail.com>
2022-09-06 22:09:53 -0500

    declared static variables because all of them belong to the whole class and I don't think I will need to create object at all. Initialized static variables in setVariables() method.

---
+++ src/state_of_java/AlenaLand.java
@@ -0,0 +1,15 @@
+package state_of_java;
+
+public class AlenaLand {
+ static int population;
+ static double area;
+ static String language;
+
+ public void setVariables(int population, double area, String language){
+    this.population = population;
+    this.area = area;
+    this.language = language;
+ }
+
+
+}
