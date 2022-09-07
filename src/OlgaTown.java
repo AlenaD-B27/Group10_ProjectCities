966730c3577a86ee6d4886593b8eda513d885f32
olhasoroka <olly0296@gmail.com>
2022-09-06 19:05:08 -0500

    variables added and initialized

--- src/OlgaTown.java
+++ src/OlgaTown.java
@@ -1,4 +1,14 @@
 public class OlgaTown {
 
+    int population;
+    double area;
+    String language;
+
+    public OlgaTown(int population, double area, String language){
+        this.population = population;
+        this.area = area;
+        this.language = language;
+    }
+
 
 }
